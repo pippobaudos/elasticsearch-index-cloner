b2eedc30d0550a526328e80d91c0caa406628bf4
Moses Mans <xxmans@gmail.com>
2015-08-31 17:05:29 +0100

    HDDAU-1063 Refactor IndexCloner project : cleans up CommandLineUtil and adds TESTs

--- src/main/java/com/roncia/elasticsearch/tools/com/roncia/elasticsearch/util/CommandLineUtil.java
+++ src/main/java/com/roncia/elasticsearch/tools/com/roncia/elasticsearch/util/CommandLineUtil.java
@@ -3,6 +3,9 @@ package com.roncia.elasticsearch.tools.com.roncia.elasticsearch.util;
 import com.roncia.elasticsearch.index.IndexRef;
 import org.apache.commons.cli.*;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * CommandLine static helper functions to manipulate command line arguments
@@ -12,23 +15,45 @@ import org.apache.commons.cli.*;
  */
 public class CommandLineUtil {
 
+    private final static Logger LOGGER = Logger.getLogger(CommandLineUtil.class.getName());
+
+    /**
+     * @param src the index reference of source
+     * @param dst the index reference of destination
+     * @param dstIndexReplicas the number if replicas to set for the new cloned index
+     * @return  the string array of arguments
+     */
+    public static String[] buildCloneArguments(IndexRef src, IndexRef dst, String dstIndexReplicas) {
+        return new String[]{
+            "-srcHost", src.getHost(), "-srcUser", src.getUser(), "-srcPwd", src.getPwd(), "-srcIndex", src.getIndexName(),
+            "-dstHost", dst.getHost(), "-dstUser", dst.getUser(), "-dstPwd", dst.getPwd(), "-dstIndex", dst.getIndexName(),
+            "-dstIndexReplicas", dstIndexReplicas
+        };
+    }
+
+    /**
+     * Builds a commandLine key pair values from an array of strings
+     *
+     * @param args the command line arguments
+     * @return the list of atomic option and value tokens
+     * @throws ParseException if unable to parse any of the given arguments against the available options
+     */
     public static CommandLine readCommandLine(String[] args) throws ParseException {
+        CommandLine cmd = null;
         CommandLineParser parser = new DefaultParser();
         Options options = createOptions();
-        CommandLine cmd;
         try {
           cmd = parser.parse(options, args);
         }
         catch (ParseException e) {
+            LOGGER.log(Level.SEVERE, "Unable to parse given arguments against the available options : " + e.getMessage());
             help(options);
-          throw e;
         }
         return cmd;
     }
 
-    private static Options createOptions() {
+    protected static Options createOptions() {
         Options options = new Options();
-        options.addOption(Option.builder("srcHost").hasArg().desc("source: host:port (e.g. localhost:9200)").required().build());
         options.addOption(Option.builder("srcHost").hasArg().desc("source: host:port (e.g. localhost:9200)").required().build());
         options.addOption(Option.builder("srcIndex").hasArg().desc("source: index name").required().build());
         options.addOption(Option.builder("srcUser").hasArg().desc("source: user authentication").build());
@@ -45,14 +70,7 @@ public class CommandLineUtil {
     private static void help(Options options) {
         HelpFormatter formatter = new HelpFormatter();
         formatter.printHelp("Main", options);
-        System.exit(0);
+        System.exit(0);//TODO: review: maybe we can throw an uncaught exceptions which can bubble up instead of a kill!
     }
 
-    public static String[] buildCloneArguments(IndexRef src, IndexRef dst, String dstIndexReplicas) {
-        return new String[]{
-                "-srcHost", src.getHost(), "-srcUser", src.getUser(), "-srcPwd", src.getPwd(), "-srcIndex", src.getIndexName(),
-                "-dstHost", dst.getHost(), "-dstUser", dst.getUser(), "-dstPwd", dst.getPwd(), "-dstIndex", dst.getIndexName(),
-                "-dstIndexReplicas", dstIndexReplicas
-        };
-    }
 }
---
+++ src/test/java/com/roncia/elasticsearch/tools/com/roncia/elasticsearch/util/CommandLineUtilTest.java
@@ -0,0 +1,61 @@
+package com.roncia.elasticsearch.tools.com.roncia.elasticsearch.util;
+
+import com.roncia.elasticsearch.index.IndexRef;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.ParseException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+
+import static com.roncia.elasticsearch.tools.com.roncia.elasticsearch.util.CommandLineUtil.*;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created on 31/08/2015.
+ * @author moses.mansaray
+ */
+public class CommandLineUtilTest {
+
+  @Rule
+  public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+  @Test
+  public void readCommandLineShouldReturnOptionsAsExpected() throws Exception {
+    IndexRef srcIndex = new IndexRef("mySrcHost", "user1", "pwd1", "myIndex");
+    IndexRef dstIndex = new IndexRef("myDstHost", "user2", "pwd2", "myIndexCopy");
+    String[] cloneArguments = buildCloneArguments(srcIndex, dstIndex, "7");
+    CommandLine commandLine = readCommandLine(cloneArguments);
+    int optionsLength = commandLine.getOptions().length;
+
+    assertEquals("Options size is not as expected", 9, optionsLength);
+    assertEquals("Src Host not as expected", "mySrcHost", commandLine.getOptionValue("srcHost"));
+    assertEquals("Src User not as expected", "user1", commandLine.getOptionValue("srcUser"));
+    assertEquals("Src Pwd not as expected", "pwd1", commandLine.getOptionValue("srcPwd"));
+    assertEquals("Src Index not as expected", "myIndex", commandLine.getOptionValue("srcIndex"));
+    assertEquals("Dst Host not as expected", "myDstHost", commandLine.getOptionValue("dstHost"));
+    assertEquals("Dst User not as expected", "user2", commandLine.getOptionValue("dstUser"));
+    assertEquals("Dst Pwd not as expected", "pwd2", commandLine.getOptionValue("dstPwd"));
+    assertEquals("Dst Index not as expected", "myIndexCopy", commandLine.getOptionValue("dstIndex"));
+    assertEquals("Replicas not as expected", "7", commandLine.getOptionValue("dstIndexReplicas"));
+
+  }
+
+  @Test
+  public void buildCloneArgumentsShouldMatchNumberOfOptions() throws Exception {
+    int optionsLength = createOptions().getOptions().size() - 1; // minus keepDstIndex which is not clone arg
+    IndexRef srcIndex = new IndexRef("srcHost", "user", "pwd", "myIndex");
+    IndexRef dstIndex = new IndexRef("dstHost", "user", "pwd", "myIndex");
+    String[] cloneArguments = buildCloneArguments(srcIndex, dstIndex, "0");
+
+    int cloneArgumentsLength = cloneArguments.length/2; // key values pairs so /2
+    assertEquals("Clone Args does not Match Options", optionsLength, cloneArgumentsLength);
+  }
+
+  @Test
+  public void shouldThrowExceptionForInvalidArguments() throws ParseException {
+    final String[] invalidArguments = {"this", "isInvalid"};
+    exit.expectSystemExit();
+    readCommandLine(invalidArguments);
+  }
+
+}
